ea0698151b428826454f3d54f6453fdb027f2725
Enzo Bat <e.batungwan@alustudent.com>
2024-06-22 20:31:40 +0200

    schedule

--- script.js
+++ script.js
@@ -1,3 +1,28 @@
+
+// See more Tracks
 document.getElementById('see-btn').addEventListener('change', function() {
     document.querySelector('.extra-tracks').style.display = this.checked ? 'grid' : 'none';
+});
+
+// Schedule
+function showTab(dayId, element) {
+    var i, tabcontent, tabbuttons;
+    
+    tabcontent = document.getElementsByClassName("tab-content");
+    for (i = 0; i < tabcontent.length; i++) {
+        tabcontent[i].classList.remove("active");
+    }
+    
+    tabbuttons = document.getElementsByClassName("tab-button");
+    for (i = 0; i < tabbuttons.length; i++) {
+        tabbuttons[i].classList.remove("active");
+    }
+    
+    document.getElementById(dayId).classList.add("active");
+    element.classList.add("active");
+}
+
+// Automatically show the first tab on page load
+document.addEventListener('DOMContentLoaded', function() {
+    showTab('day1', document.querySelector('.tab-button'));
 });
